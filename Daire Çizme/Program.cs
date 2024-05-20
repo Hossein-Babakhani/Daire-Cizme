7c4d60550ad9fa6861d4e4217ce80a61ca9dd508
Hossein Babakhani <bna.babakhani@gmail.com>
2024-05-20 20:46:05 +0300

    Add files via upload

---
+++ Daire Çizme/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Daire_Çizme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Çemberin yarıçapını girin:");
+            int yarıçap = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < yarıçap*2+1; i++)
+            {
+                for (int j = 0; j < yarıçap * 2 + 1; j++)
+                {
+                    if((i- yarıçap)* (i - yarıçap)+(j- yarıçap)* (j - yarıçap)<= yarıçap* yarıçap)
+                    {
+                        Console.Write("*");
+                    }
+                    else
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
