8e3aecba0f876d2fe096b70bde834d36cd51e96b
Hossein Babakhani <bna.babakhani@gmail.com>
2024-05-21 11:26:42 +0300

    Update Program.cs

--- Daire Çizme/Program.cs
+++ Daire Çizme/Program.cs
@@ -6,13 +6,13 @@ namespace Daire_Çizme
     {
         static void Main(string[] args)
         {
-            Console.Write("Çemberin yarıçapını girin:");
-            int yarıçap = Convert.ToInt32(Console.ReadLine());
-            for (int i = 0; i < yarıçap*2+1; i++)
+            Console.Write("Enter the radius of the circle:");
+            int radius = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < radius*2+1; i++)
             {
-                for (int j = 0; j < yarıçap * 2 + 1; j++)
+                for (int j = 0; j < radius * 2 + 1; j++)
                 {
-                    if((i- yarıçap)* (i - yarıçap)+(j- yarıçap)* (j - yarıçap)<= yarıçap* yarıçap)
+                    if((i- radius)* (i - radius)+(j- radius)* (j - radius)<= radius* radius)
                     {
                         Console.Write("*");
                     }
